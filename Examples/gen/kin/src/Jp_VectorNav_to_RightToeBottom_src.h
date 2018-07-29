69200f42a51677b5930c5cb666235acb3861ba6b
Ayonga Hereid <ayonga27@gmail.com>
2018-07-29 04:04:23 -0400

    updated generated functions for Linux.

--- Examples/gen/kin/src/Jp_VectorNav_to_RightToeBottom_src.h
+++ Examples/gen/kin/src/Jp_VectorNav_to_RightToeBottom_src.h
@@ -1,10 +1,10 @@
 /*
  * Automatically Generated from Mathematica.
- * Tue 17 Jul 2018 10:14:16 GMT-04:00
+ * Sun 29 Jul 2018 03:58:22 GMT-04:00
  */
 
-#ifndef JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_HH
-#define JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_HH
+#ifndef JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_H
+#define JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_H
 
 #ifdef MATLAB_MEX_FILE
 #include <tmwtypes.h>
@@ -14,4 +14,5 @@
 
 void Jp_VectorNav_to_RightToeBottom_src(double *p_output1, const double *var1);
 
-#endif // JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_HH
+#endif 
+/* JP_VECTORNAV_TO_RIGHTTOEBOTTOM_SRC_H */
